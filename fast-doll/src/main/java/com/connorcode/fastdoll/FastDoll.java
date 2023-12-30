4cba35e8b521a54ee6f6d7ac752cde427b737a81
Connor Slade <connor@connorcode.com>
2023-12-30 18:32:35 -0500

    FastDoll: Bump version to 1.3

--- fast-doll/src/main/java/com/connorcode/fastdoll/FastDoll.java
+++ fast-doll/src/main/java/com/connorcode/fastdoll/FastDoll.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public class FastDoll implements ClientModInitializer {
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
     public static final MinecraftClient client = MinecraftClient.getInstance();
     public static final Path config = client.runDirectory.toPath().resolve("config/fast-doll.nbt");
     public static boolean enabled = true;
