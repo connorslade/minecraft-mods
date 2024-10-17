15ec96aa64292bc82434a10387ee304d7b092590
Connor Slade <connor@connorcode.com>
2024-10-17 12:19:24 -0400

    Update to 1.20.x (Fixes #2)

--- fast-doll/src/main/java/com/connorcode/fastdoll/FastDoll.java
+++ fast-doll/src/main/java/com/connorcode/fastdoll/FastDoll.java
@@ -9,13 +9,15 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public class FastDoll implements ClientModInitializer {
-    public static final String VERSION = "1.7";
+    public static final String VERSION = "1.8";
     public static final MinecraftClient client = MinecraftClient.getInstance();
-    public static final Path config = client.runDirectory.toPath().resolve("config/fast-doll.nbt");
+    public static final Path configDir = client.runDirectory.toPath().resolve("config");
+    public static final Path config = configDir.resolve("fastdoll.json");
     public static boolean enabled = true;
 
     @Override
     public void onInitializeClient() {
+        configDir.toFile().mkdirs();
         if (!config.toFile().exists()) saveConfig();
         else loadConfig();
     }
