3271bbea5954a5aa5f68b8060e4b07c9138e711c
Connor Slade <connor@connorcode.com>
2024-10-26 20:06:48 -0400

    FastDoll: Bump version to 1.9

--- fast-doll/src/main/java/com/connorcode/fastdoll/FastDoll.java
+++ fast-doll/src/main/java/com/connorcode/fastdoll/FastDoll.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public class FastDoll implements ClientModInitializer {
-    public static final String VERSION = "1.8";
+    public static final String VERSION = "1.9";
     public static final MinecraftClient client = MinecraftClient.getInstance();
     public static final Path configDir = client.runDirectory.toPath().resolve("config");
     public static final Path config = configDir.resolve("fastdoll.json");
