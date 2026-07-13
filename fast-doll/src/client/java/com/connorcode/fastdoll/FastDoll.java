cfcc21529beae47b4593fb627bfa3ae0538c2fe8
Connor Slade <connor@connorcode.com>
2026-07-13 01:01:12 -0400

    Fast Doll: Update for Minecraft 26.1.1

--- fast-doll/src/client/java/com/connorcode/fastdoll/FastDoll.java
+++ fast-doll/src/client/java/com/connorcode/fastdoll/FastDoll.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public class FastDoll implements ClientModInitializer {
-    public static final String VERSION = "1.18";
+    public static final String VERSION = "1.19";
     public static final Minecraft client = Minecraft.getInstance();
     public static final Path configDir = client.gameDirectory.toPath().resolve("config");
     public static final Path config = configDir.resolve("fastdoll.json");
